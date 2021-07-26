b6446bf31a36dafc50f487ec079d14d41934baef
petchkubbb <petchkubb@live.com>
2021-07-26 09:05:35 +0700

    setstate login form

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -20,32 +20,7 @@ class Login extends StatelessWidget {
                     image: AssetImage('images/logo.png'),
                   ),
                 ),
-                Container(
-                  child: TextField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(), labelText: 'Username'),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 16),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(), labelText: 'Password'),
-                  ),
-                ),
-                Container(
-                  width: double.infinity,
-                  height: 44,
-                  margin: EdgeInsets.symmetric(vertical: 8),
-                  child: ElevatedButton(
-                    child: Text('Login'),
-                    style: ButtonStyle(
-                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0),
-                    ))),
-                    onPressed: () {},
-                  ),
-                ),
+                LoginForm(),
                 Container(
                   width: double.infinity,
                   height: 44,
@@ -63,6 +38,73 @@ class Login extends StatelessWidget {
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({Key? key}) : super(key: key);
+
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final userNameController = TextEditingController();
+  final passWordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    userNameController.dispose();
+    passWordController.dispose();
+    super.dispose();
+  }
+
+  void _onLogin() {
+    print('username:${userNameController.text}');
+    print('password:${passWordController.text}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            child: TextField(
+              controller: userNameController,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Username'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 16),
+            child: TextField(
+              controller: passWordController,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Password'),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 44,
+            margin: EdgeInsets.symmetric(vertical: 8),
+            child: ElevatedButton(
+              child: Text('Login'),
+              style: ButtonStyle(
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
+              ))),
+              onPressed: _onLogin,
+            ),
+          ),
+        ],
       ),
     );
   }
