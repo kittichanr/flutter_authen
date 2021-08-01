3e87faf91707b8e4f061aa09bda84be977be5691
petchkubbb <petchkubb@live.com>
2021-08-01 13:26:23 +0700

    fix catch password signup

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -63,11 +63,15 @@ class _SignUpState extends State<SignUp> {
           Navigator.of(context).pop();
         }
       } on FirebaseAuthException catch (e) {
-        if (e.code == 'weak-password') {
-          print('The password provided is too weak.');
-        } else if (e.code == 'email-already-in-use') {
-          print('The account already exists for that email.');
-        }
+        setState(() {
+          if (e.code == 'weak-password') {
+            _passwordErrorText = 'The password provided is too weak.';
+            print('The password provided is too weak.');
+          } else if (e.code == 'email-already-in-use') {
+            _passwordErrorText = 'The account already exists for that email.';
+          }
+          _isProcessing = false;
+        });
       }
     }
   }
