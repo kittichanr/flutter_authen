a50c1e7c7f9185e523f4e88404108d607cea84cc
petchkubbb <petchkubb@live.com>
2021-09-04 08:48:24 +0700

    add save and delete file

--- lib/providers/ImageProvider.dart
+++ lib/providers/ImageProvider.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_authen/modules/fire_storage.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:intl/intl.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:firebase_core/firebase_core.dart' as firebase_core;
 
 class ImgProvider with ChangeNotifier {
   List<Map<String, dynamic>> _selectedList = [];
@@ -8,14 +14,43 @@ class ImgProvider with ChangeNotifier {
   late Future<List<Map<String, dynamic>>> _imageList;
   Future<List<Map<String, dynamic>>> get imageList => _imageList;
   void fetchImageList() {
+    notifyListeners();
     _imageList = FireStorage().getImageList();
   }
 
   void addSelectImage(Map<String, dynamic> image) {
+    notifyListeners();
     _selectedList.add(image);
   }
 
   void removeSelectImage(Map<String, dynamic> image) {
+    notifyListeners();
     _selectedList.remove(image);
+  }
+
+  void deleteSelectedImage() async {
+    notifyListeners();
+    for (var i = 0; i < _selectedList.length; i++) {
+      FireStorage().deleteImage(_selectedList[i]['path']);
+    }
+    _selectedList = [];
+  }
+
+  void saveSelectedImage() async {
+    notifyListeners();
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    File downloadToFile = File(
+        '${appDocDir.path}/beepees-${DateFormat("yyyy-MM-dd HH:mm:ss").format(DateTime.now())}.png');
+    try {
+      for (var i = 0; i < _selectedList.length; i++) {
+        await firebase_storage.FirebaseStorage.instance
+            .ref(_selectedList[i]['path'])
+            .writeToFile(downloadToFile);
+      }
+      _selectedList = [];
+      fetchImageList();
+    } on firebase_core.FirebaseException catch (e) {
+      print(e);
+    }
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_authen/modules/image_picker.dart';
+import 'package:flutter_authen/providers/ImageProvider.dart';
 import 'package:flutter_authen/widgets/drawerList.dart';
 import 'package:flutter_authen/widgets/imageList.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,9 +15,37 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    ImgProvider imageProvider = Provider.of<ImgProvider>(context);
+    List<Map<String, dynamic>> selectedList = imageProvider.selectedList;
+
     return Scaffold(
       appBar: AppBar(
+        centerTitle: false,
         title: Text("Beepee's Cactus"),
+        actions: selectedList.length > 0
+            ? <Widget>[
+                Padding(
+                    padding: EdgeInsets.only(right: 20.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        imageProvider.saveSelectedImage();
+                      },
+                      child: Icon(
+                        Icons.save,
+                        size: 26.0,
+                      ),
+                    )),
+                Padding(
+                    padding: EdgeInsets.only(right: 20.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        imageProvider.deleteSelectedImage();
+                        imageProvider.fetchImageList();
+                      },
+                      child: Icon(Icons.delete),
+                    )),
+              ]
+            : [],
       ),
       drawer: Drawer(
         child: DrawerList(),
